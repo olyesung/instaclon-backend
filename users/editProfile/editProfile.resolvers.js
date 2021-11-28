cbb9856a0b232e53b1d8af6d36a8a6bb1908ebf1
olyesung <olyesung@gmail.com>
2021-11-28 22:17:42 +0900

    2.17 File Upload part Four

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,7 +1,10 @@
+import { createWriteStream } from "fs";
 import bcrypt from "bcrypt";
 import client from "../../client";
 import { protectedResolver } from "../users.utils";
 import { GraphQLUpload } from "graphql-upload";
+
+console.log(process.cwd());
 
 const resolverFn = async (
   _,
@@ -9,8 +12,9 @@ const resolverFn = async (
   { loggedInUser }
 ) => {
   const { filename, createReadStream } = await avatar;
-  const stream = createReadStream();
-  console.log(stream);
+  const readStream = createReadStream();
+  const writeStream = createWriteStream(process.cwd() + "/uploads/" + filename);
+  readStream.pipe(writeStream);
   let uglyPassword = null;
   if (newPassword) {
     uglyPassword = await bcrypt.hash(newPassword, 10);
