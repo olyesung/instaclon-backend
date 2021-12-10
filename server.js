adcc65af1998d3d853012403ffd02b85507c48ce
olyesung <olyesung@gmail.com>
2021-12-11 00:22:13 +0900

    4.7 Subscriptions Setup part One

--- server.js
+++ server.js
@@ -1,31 +1,53 @@
 require("dotenv").config();
+import { createServer } from "http";
 import { ApolloServer } from "apollo-server-express";
 import { graphqlUploadExpress } from "graphql-upload";
+import { SubscriptionServer } from "subscriptions-transport-ws";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { execute, subscribe } from "graphql";
 import express from "express";
 import logger from "morgan";
 import { resolvers, typeDefs } from "./schema";
 import { getUser } from "./users/users.utils";
+import pubsub from "./pubsub";
 
 const PORT = process.env.PORT;
 
 async function startServer() {
+  const app = express();
+  const httpServer = createServer(app);
+  app.use(graphqlUploadExpress());
+  app.use(logger("tiny"));
+  app.use("/static", express.static("uploads"));
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const subscriptionServer = SubscriptionServer.create(
+    { schema, execute, subscribe },
+    { server: httpServer, path: "/graphql" }
+  );
   const apollo = new ApolloServer({
-    resolvers,
-    typeDefs,
+    schema,
     context: async ({ req }) => {
       return {
         loggedInUser: await getUser(req.headers.token),
       };
     },
+    plugins: [
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              subscriptionServer.close();
+            },
+          };
+        },
+      },
+    ],
   });
 
   await apollo.start();
-  const app = express();
-  app.use(graphqlUploadExpress());
-  app.use(logger("tiny"));
   apollo.applyMiddleware({ app });
-  app.use("/static", express.static("uploads"));
-  app.listen({ port: PORT }, () => {
+
+  httpServer.listen(PORT, () => {
     console.log(`🚀Server is running on http://localhost:${PORT} ✅`);
   });
 }
